1d1065ddf009d2e8f3db974b68f4d468d5d9d8e0
root <root@DESKTOP-21614UA.>
2026-05-14 00:39:07 +0800

    Fix

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ nothing:
 
 # NEWPROBELM
 
-merge-pdfs: pA/attachments/problems.pdf
+merge-pdfs: /attachments/problems.pdf
 
-pA/attachments/problems.pdf: p*/statement/index.pdf
+pA/attachments/problems.pdf: */statement/index.pdf
 	bash merge_pdfs.sh
--- merge_pdfs.sh
+++ merge_pdfs.sh
@@ -4,12 +4,24 @@ export PROBLEMSJSON=$(cat .problems.json)
 PROBLEMS=$(python3 -c 'import json, os; print(" ".join(json.loads(os.environ.get("PROBLEMSJSON"))))')
 
 MERGECMD="pdfunite "
+
+if [-f "cover.tex"]; then
+	xelatex cover.tex
+	MERGECMD = "$MERGECMD cover.pdf"
+fi
+
 for prob in $PROBLEMS; do
-	if [ -f "p${prob}/statement/index.pdf" ]; then
-		MERGECMD="$MERGECMD p${prob}/statement/index.pdf"
+	if [ -f "${prob}/statement/index.pdf" ]; then
+		MERGECMD="$MERGECMD ${prob}/statement/index.pdf"
 	fi
 done
+
+if [-f "appendix.tex"]; then
+	xelatex appendix.tex
+	MERGECMD = "$MERGECMD appendix.pdf"
+fi
+
 MERGECMD="$MERGECMD pA/attachments/problems.pdf"
 echo $MERGECMD
-mkdir -p pA/attachments
+mkdir -p attachments
 $MERGECMD
